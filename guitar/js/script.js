d20359030800d03c5d867fb047d7a89127ca6a11
kostukovich.d@gmail.com <kostukovich.d@gmail.com>
2022-05-20 22:45:47 +0300

    add guitar

---
+++ guitar/js/script.js
@@ -0,0 +1,28 @@
+AOS.init();
+$(document).ready(function(){
+    
+    var activeClass = 'slick-active',
+        ariaAttribute = 'aria-hidden';
+    $( '.custom_inner' )
+    .on( 'init', function() {
+        $( '.slick-dots li:first-of-type' ).addClass( activeClass ).attr( ariaAttribute, false );
+    } )
+    .on( 'afterChange', function( event, slick, currentSlide ) {
+        var $dots = $( '.slick-dots' );
+        $( 'li', $dots ).attr( ariaAttribute, true );
+        $dots.each( function() {
+            $( 'li', $( this ) ).eq( currentSlide ).addClass( activeClass ).attr( ariaAttribute, false );
+        } );
+    } );
+    $('.custom_inner').slick({
+        dots: true,
+        appendDots: '.custom_inner_wrapper_text_descr',
+        arrows: false,
+        infinite: true,
+        speed: 700,
+        cssEase: 'linear'
+      });
+      
+
+    
+});
